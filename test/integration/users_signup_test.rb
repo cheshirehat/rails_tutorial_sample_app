c51218122588ccf487298807de48bb368b09bb9e
cheshirehat <cheshireinhatter@gmail.com>
2018-09-26 22:49:55 +0900

    fix

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,7 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information" do
     get signup_path
-    assert_diffetence 'User.count', 1 do
+    assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User",
         email: "user@examle.com",
         password: "password",
